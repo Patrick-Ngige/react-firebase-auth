8641686f5279deebfa48b299c4f2e7b83282fc49
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-17 00:42:35 +0300

    AuthUser component setup

--- src/components/AuthDetails.jsx
+++ src/components/AuthDetails.jsx
@@ -1,14 +1,22 @@
+import { onAuthStateChanged } from 'firebase/auth'
 import React, { useEffect, useState } from 'react'
+import { auth } from '../firebase'
 
 const AuthDetails = () => {
-    const [user, setUser] = useState(null)
+    const [authUser, setAuthUser] = useState(null)
 
     useEffect(() => {
-        
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser(user)
+            } else {
+                setAuthUser(null)
+            }
+        })
     }, [])
   return (
     <div>
-      
+      {authUser ? <p>Signed In</p> : <p>Signed Out</p>}
     </div>
   )
 }
