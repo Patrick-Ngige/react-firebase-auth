6be53d322e1e4e8ef57bf936cae62d99e9441a5d
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-17 00:01:05 +0300

    sign in component

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react'
+import {auth} from '../../firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const SignIn = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const signIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {} )
+    }
+
   return (
     <div className='sign-in-container'>
-      <form>
+      <form onSubmit={signIn}>
         <h1>Log In</h1>
-        <input type='email' placeholder='Enter your email' value={email}/>
-        <input type='password' placeholder='Enter your password' value={password}/>
+        <input type='email' placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type='password' placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+      
+      <button type='submit'>Log In</button>
       </form>
     </div>
   )
